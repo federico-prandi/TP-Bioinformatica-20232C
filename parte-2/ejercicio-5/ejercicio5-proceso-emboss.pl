c3d85d2e3f82fbf985d04d0c3c55d9c94625296c
federico-prandi <federico.prandi@mercadolibre.com>
2023-11-27 20:13:34 -0300

    corrections of patmatmotifs forgotted

--- parte-2/ejercicio-5/ejercicio5-proceso-emboss.pl
+++ parte-2/ejercicio-5/ejercicio5-proceso-emboss.pl
@@ -2,53 +2,64 @@
 
 use strict;
 use warnings;
-
-# Archivo de entrada de nucléotidos
+# Input nucleotide sequence file
 my $input_fasta_file = "MYBPC3-mNRA-sequence.fasta";
 
-# Ruta de la base de datos de dominios y funcionalidades
+# Path to the Prosite domain and functionality database
 my $prosite_db_path = "prosite.dat";
 
-# Programa para obtener los ORF's
+# Run getorf to obtain ORFs from the input sequence
 my $orf_output = `getorf -sequence $input_fasta_file -outseq orfs.fasta`;
 
 if ($? == 0) {
-    print "ORFs identificado correctamente .\n";
+    print "ORFs identified successfully.\n";
 
-    # Ejecuta el análisis de dominios / funcionalidad por cada ORF
-    my $orf_sequence = '';
-    open(my $orf_file, '<', 'orfs.fasta') or die "Could not open ORF file: $!";
-    while (<$orf_file>) {
-        if (/^>ORF_(\d+)/) {
-            my $orf_num = $1;
-            if ($orf_sequence ne '') {
-                # Run patmatmotifs on the ORF sequence
-                my $output = `patmatmotifs -sequence -outfile motifs_orf_$orf_num.txt -d $prosite_db_path`;
+    # Execute domain/functional analysis for each ORF
+open(my $orf_file, '<', 'orfs.fasta') or die "Could not open ORF file: $!";
+my $orf_num;
+my $orf_sequence = '';
 
-                # Validación de análisis de ORF
-                if ($? == 0) {
-                    print "Análisis de ORF $orf_num completado satisfactoriamente.\n";
+while (<$orf_file>) {
+    if (/^>(NM_\d+\.\d+_cds_NP_\d+\.\d+_\d+_\d+)/) {
+        $orf_num = $1;
+        if ($orf_sequence ne '') {
+            # Create a temporary file to store the ORF sequence
+            my $tmp_sequence_file = "tmp_sequence_$orf_num.fasta";
+            open(my $tmp_sequence_fh, '>', $tmp_sequence_file) or die "Could not open temporary sequence file: $!";
+            print $tmp_sequence_fh ">$orf_num\n$orf_sequence";
+            close $tmp_sequence_fh;
 
-                    # Process the results for this ORF (you can modify this part according to your needs)
-                    open(my $motif_file, '<', "motifs_orf_$orf_num.txt") or die "No pudo abrir el archivo de análisis para ORF $orf_num: $!";
-                    while (<$motif_file>) {
-                        if (/^(query\s+.+)$/) {
-                            print "ORF $orf_num Motif: $1\n";
-                        }
+            # Run patmatmotifs on the temporary sequence file
+            my $output = `patmatmotifs -sequence $tmp_sequence_file -outfile motifs_$orf_num.txt`;
+
+
+            # Remove the temporary sequence file
+            unlink $tmp_sequence_file;
+
+            # Validation of ORF analysis
+            if ($? == 0) {
+                print "Analysis for ORF $orf_num completed successfully.\n";
+
+                # Process the results for this ORF (modify according to your needs)
+                open(my $motif_file, '<', "motifs_$orf_num.txt") or die "Could not open analysis file for ORF $orf_num: $!";
+                while (<$motif_file>) {
+                    if (/^(query\s+.+)$/) {
+                        print "ORF $orf_num Motif: $1\n";
                     }
-                    close $motif_file;
-                } else {
-                    print "Error ejecutando patmatmotifs para el ORF $orf_num: $!\n";
                 }
+                close $motif_file;
+            } else {
+                print "Error executing patmatmotifs for ORF $orf_num: $!\n";
             }
-
-            # Resete de secuencia de ORF sequence
-            $orf_sequence = '';
-        } else {
-            $orf_sequence .= $_;
         }
+
+        # Reset ORF sequence
+        $orf_sequence = '';
+    } else {
+        $orf_sequence .= $_;
     }
-    close $orf_file;
+}
+close $orf_file;
 } else {
-    print "Error ejecutando getorf: $!\n";
+    print "Error executing getorf: $!\n";
 }
